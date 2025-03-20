de56e2b19142396a9ae8c0c95ac556494df48015
James Stammers <James.h.stammers@gmail.com>
2025-03-20 23:19:21 +0100

    Fixed conversions

--- index.js
+++ index.js
@@ -18,25 +18,25 @@ function renderOutput(value) {
 }
 
 function renderLength(value) {
-    let meters = value * 0.3048
-    let feet = value * 3.28084
-    lengthOutput.textContent = makeConversionString(value, 'meters', 'feet', feet, meters);
+    let meters = value * 0.3048;  // 1 foot = 0.3 meters
+    let feet = value * 3.28084;
+    lengthOutput.textContent = makeConversionString(value, 'meters', 'feet', meters, feet);
 }
 
 function renderVolume(value) {
-    let liters = value * 4.54609
-    let gallons = value * 0.219969
-    volumeOutput.textContent = makeConversionString(value, 'liters', 'gallons', gallons, liters);
+    let liters = value * 4.54609;
+    let gallons = value * 0.219969;
+    volumeOutput.textContent = makeConversionString(value, 'liters', 'gallons', liters, gallons);
 }
 
 function renderMass(value) {
-    let kilos = value * 2.20462
-    let pounds = value * 0.453592
-    massOutput.textContent = makeConversionString(value, 'kilos', 'pounds', pounds, kilos);
+    let kilos = value * 0.453592;
+    let pounds = value * 2.20462;
+    massOutput.textContent = makeConversionString(value, 'kilos', 'pounds', kilos, pounds);
 }
 
 function makeConversionString(input, unitA, unitB, conversionA, conversionB) {
-    let result = `${input} ${unitA} = ${conversionB.toFixed(2)} / ${input} ${unitB} = ${conversionA.toFixed(2)}`
+    let result = `${input} ${unitA} = ${conversionB.toFixed(2)} ${unitB} / ${input} ${unitB} = ${conversionA.toFixed(2)} ${unitA}`
     console.log(result)
     return result
 }
